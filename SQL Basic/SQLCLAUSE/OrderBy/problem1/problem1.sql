c78f5c1fe8f412a0ea44810a3d445caef532ce78
JangamHemaSekharaiah <sekharhema0001@gmail.com>
2026-04-27 04:46:20 +0530

    problem 2

---
+++ SQL Basic/SQLCLAUSE/OrderBy/problem1/problem1.sql
@@ -0,0 +1,18 @@
+use pratice;
+show tables;
+
+create table stduent(id INT, name VARCHAR(50), department VARCHAR(10), salary INT);
+
+INSERT INTO stduent (id, name, department, salary) VALUES
+(1, 'Ram', 'A', 5000),
+(2, 'Sam', 'B', 6000),
+(3, 'Rita', 'C', 7000),
+(4, 'John', 'A', 5500),
+(5, 'Anu', 'B', 6500),
+(6, 'Ravi', 'C', 6500),
+(7, 'Meena', 'A', 6500);
+
+delete from stduent;
+
+select * from stduent;
+
