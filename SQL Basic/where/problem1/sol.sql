4199a6601944d0b6be8e26686e591e277595ef4e
JangamHemaSekharaiah <sekharhema0001@gmail.com>
2026-04-26 22:02:38 +0530

    problem 1

---
+++ SQL Basic/where/problem1/sol.sql
@@ -0,0 +1,13 @@
+create table stduent(id INT, name VARCHAR(50), department VARCHAR(10), salary INT);
+
+INSERT INTO stduent (id, name, department, salary) VALUES
+(1, 'Ram', 'A', 5000),
+(2, 'Sam', 'B', 6000),
+(3, 'Rita', 'C', 7000),
+(4, 'John', 'A', 5500),
+(5, 'Anu', 'B', 6500);
+
+select * from stduent;
+
+select stduent.name,salary from stduent
+where salary=5000;
